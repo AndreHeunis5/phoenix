4ddee50f846453bad09580f7df7ff7a8a98c387e
Andre Heunis <andreheunis5@gmail.com>
2018-08-18 09:21:48 +1000

    basic backtest with one stock

---
+++ backtest/simple_backtest.py
@@ -0,0 +1,69 @@
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+import datetime
+import backtrader as bt
+# import pyfolio as pf
+
+from backtest.strategy.TestStrategy import TestStrategy
+
+
+if __name__ == '__main__':
+    # Create a cerebro entity
+    cerebro = bt.Cerebro()
+
+    # Create a Data Feed
+    data0 = bt.feeds.YahooFinanceCSVData(
+        dataname='/Users/andreheunis/python_projects/phoenix/data/repo/orcl-1995-2014.txt',
+        fromdate=datetime.datetime(2000, 1, 1),
+        todate=datetime.datetime(2000, 12, 31),
+        reverse=False)
+    cerebro.adddata(data0, name='data0')
+
+    # data1 = bt.feeds.YahooFinanceCSVData(
+    #     dataname='/Users/andreheunis/python_projects/phoenix/data/repo/yhoo-1996-2014.txt',
+    #     fromdate=datetime.datetime(2000, 1, 1),
+    #     todate=datetime.datetime(2000, 12, 31),
+    #     reverse=False)
+    # cerebro.adddata(data1, name='data1')
+
+    # Initialise the backtest
+    cerebro.addstrategy(TestStrategy)
+    cerebro.broker.setcash(10000.0)
+    cerebro.addsizer(bt.sizers.FixedSize, stake=10)
+    cerebro.broker.setcommission(commission=0.0)
+
+    # cerebro.addanalyzer(bt.analyzers.PyFolio, _name='pyfolio')
+
+    # Print out the starting conditions
+    print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
+
+    # Run over everything
+    results = cerebro.run()
+
+    # Print out the final result
+    print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
+
+    # strat = results[0]
+    # pyfoliozer = strat.analyzers.getbyname('pyfolio')
+
+    # returns, positions, transactions, gross_lev = pyfoliozer.get_pf_items()
+
+    # print('-- RETURNS')
+    # print(returns)
+    # print('-- POSITIONS')
+    # print(positions)
+    # print('-- TRANSACTIONS')
+    # print(transactions)
+    # print('-- GROSS LEVERAGE')
+    # print(gross_lev)
+
+    # pf.create_full_tear_sheet(
+    #     returns,
+    #     positions=positions,
+    #     transactions=transactions,
+    #     live_start_date='2000-01-01',
+    #     round_trips=True)
+
+    # Plot the result
+    cerebro.plot()
